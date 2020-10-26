ce7b91f5ed584abc29dc93aab4d9e3b3651279b8
Ngazetungue Muheue <nmuheuez@gmail.com>
2020-10-26 23:30:04 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,6 +4,9 @@ print("I am bored again")
 print("I am bored again"
 print(" NOTHING Special here")
 
+print("I am bored again")
+print("I am bored again")
+
 
 print(" NOTHING Special here")
 mail_admins
