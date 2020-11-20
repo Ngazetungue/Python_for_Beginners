4b9e4511bfcc02b2de74bf6bb539fac0f187927b
Ngazetungue Muheue <nmuheuez@gmail.com>
2020-11-20 23:10:21 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,5 +13,10 @@ print("I am bored again"
 print("I am bored again"
 
 print("I am bored again"
+print("I am bored again"
+print("I am bored again"
+print("I am bored again"
+
+print("I am bored again"
 
 print("I am bored again"
